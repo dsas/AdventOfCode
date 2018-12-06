62b8ae6b41dfbe5952e40ad20096acda0c7db356
Dean Sas <dean@deansas.org>
2018-12-06 00:22:28 +0000

    Improved comments

--- 2018/day5.php
+++ 2018/day5.php
@@ -1,12 +1,24 @@
 <?php
 
 /**
- * Remove adjacent values of the same value in the opposite case
- * e.g. dabAcCaCBAcCcaDA becomes dabCBAcaDA
+ * Prints the length of a polymer fully reacted
  *
- * The values are a-Z (obviously) and are expected on STDIN
+ * Prints the length of the shortest polymer after stripping all units
+ * of a type.
+ *
+ * Reacting a polymer means removing all letters that are adjacent to
+ * the same letter of the opposite case.
+ *
+ * input is expected to be a single line on STDIN
  */
 
+/**
+ * Remove letters adjacent to the same letters of the opposite case
+ * e.g. dabAcCaCBAcCcaDA becomes dabCBAcaDA
+ *
+ * @param string $polymer a-Z
+ * @return string reacted poymer
+ */
 function react($polymer)
 {
     $reacted = [];
