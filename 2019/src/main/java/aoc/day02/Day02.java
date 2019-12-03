3920b0408873779638d56737c70a1ad5105e8be5
Dean Sas <dean@deansas.org>
2019-12-03 21:53:05 +0000

    2019 Day 2 part two

--- 2019/src/main/java/aoc/day02/Day02.java
+++ 2019/src/main/java/aoc/day02/Day02.java
@@ -3,6 +3,7 @@ package aoc.day02;
 import aoc.Day;
 import aoc.ShipComputer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,18 +21,32 @@ public class Day02 implements Day {
         ShipComputer computer = new ShipComputer();
         program = computer.ExecuteProgram(program);
 
-
         return program.get(0).toString();
     }
 
     @Override
     public String part2(List<String> input) {
+        List<Integer> resultState;
+        List<Integer> initialState = CsvLineToInts(input.get(0));
+        List<Integer> program;
+        ShipComputer computer = new ShipComputer();
 
-        return "";
+        for (int i = 0; i < 100; i++) {
+           for (int j = 0; j < 100; j++) {
+               program = new ArrayList<>(initialState);
+               program.set(1, i);
+               program.set(2, j);
+
+               resultState = computer.ExecuteProgram(program);
+               if (resultState.get(0) == 19690720) {
+                   return Integer.toString(100 * i + j);
+               }
+           }
+        }
+        throw new RuntimeException("Up the range");
     }
 
     private List<Integer> CsvLineToInts(String line) {
-
         return Stream.of(line.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
