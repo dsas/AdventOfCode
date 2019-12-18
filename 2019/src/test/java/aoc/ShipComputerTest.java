55e9169f49fcfd680eedce42ddb7872f494d1499
Dean Sas <dean@deansas.org>
2019-12-18 22:57:15 +0000

    Refactor tests for ship computer (day 2)

--- 2019/src/test/java/aoc/ShipComputerTest.java
+++ 2019/src/test/java/aoc/ShipComputerTest.java
@@ -13,30 +13,30 @@ import static org.junit.Assert.assertEquals;
 public class ShipComputerTest {
 
     @Test
-    public void executeProgram(){
-
-        ShipComputer computer = new ShipComputer();
-
-        // I feel bad but it's easier to declare this list of tests and deal with the boxing than the alternative
-        int[][][] testCases = {
-                {{1,0,0,0,99}, {2,0,0,0,99}},
-                {{2,3,0,3,99}, {2,3,0,6,99}},
-                {{2,4,4,5,99,0}, {2,4,4,5,99,9801}},
-                {{1,1,1,4,99,5,6,0,99}, {30,1,1,4,2,5,6,0,99}}};
-
-        for (int[][] testCase : testCases) {
-            // Given
-            List<Integer> program = intToInteger(testCase[0]);
-
-            // When
-            List<Integer> result = computer.ExecuteProgram(program);
-
-            // Then
-            assertEquals(intToInteger(testCase[1]), result);
-        }
+    public void addNumbers() {
+        // 1 + 1 = 2
+        testComputer(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 99)), new ArrayList<>(Arrays.asList(2, 0, 0, 0, 99)));
     }
 
-    private List<Integer> intToInteger(int[] intArray) {
-        return Arrays.stream(intArray).boxed().collect(Collectors.toList());
+    @Test
+    public void multiplyNumbers() {
+        // 3 * 2 = 6
+        testComputer(new ArrayList<>(Arrays.asList(2, 3, 0, 3, 99)), new ArrayList<>(Arrays.asList(2, 3, 0, 6, 99)));
+        // 99 * 99 = 9801
+        testComputer(new ArrayList<>(Arrays.asList(2, 4, 4, 5, 99, 0)), new ArrayList<>(Arrays.asList(2, 4, 4, 5, 99, 9801)));
+    }
+
+    @Test
+    public void replacingHaltContinues() {
+        // 1+1=2, store in place of the first 99
+        // 5*6 = 30 store in place of the first 1
+        testComputer(new ArrayList<>(Arrays.asList(1, 1, 1, 4, 99, 5, 6, 0, 99)), new ArrayList<>(Arrays.asList(30, 1, 1, 4, 2, 5, 6, 0, 99)));
+    }
+
+    public void testComputer(List<Integer> program, List<Integer> expectedResult) {
+        ShipComputer computer = new ShipComputer();
+
+        List<Integer> actualResult = computer.ExecuteProgram(program);
+        assertEquals(expectedResult, actualResult);
     }
 }
