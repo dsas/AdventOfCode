304774b045cf1712ab6351828a6a12e2d7930a73
Dean Sas <dean@deansas.org>
2020-11-11 22:50:00 +0000

    2019: Refactor day 5 and implement part two

--- 2019/src/main/java/aoc/day05/Day05.java
+++ 2019/src/main/java/aoc/day05/Day05.java
@@ -12,33 +12,46 @@ public class Day05 implements Day {
 
     @Override
     public String part1(List<String> input) {
-
+        int diagnosticCode;
         // Provided program will run a series of diagnostic tests on the ShipComputer
         List<Integer> thermalDiagnosticProgram = CsvLineToInts(input.get(0));
 
         // Tell the program to test the ships aircon unit (system 1)
-        LinkedList<Integer> inputInstruction = new LinkedList<>();
-        inputInstruction.add(1);
-
-        ShipComputer computer = new ShipComputer(inputInstruction);
-        computer.executeProgram(thermalDiagnosticProgram);
-
-        LinkedList<Integer> thermalDiagnosticOutput = computer.outputBuffer();
-
-        // The system will output the result of each test and finally it will output a
-        // diagnostic code and halt.
-        int diagnosticCode = thermalDiagnosticOutput.removeLast();
-
-        if (!this.checkDiagnosticSuccessful(thermalDiagnosticOutput)) {
-            throw new RuntimeException("Test failure " + thermalDiagnosticOutput.toString());
-        }
+        diagnosticCode = runDiagnosticsOnSystem(thermalDiagnosticProgram, 1);
 
         return Integer.toString(diagnosticCode);
     }
 
     @Override
     public String part2(List<String> input) {
-        throw new RuntimeException("Not implemented yet");
+        int diagnosticCode;
+        // Provided program will run a series of diagnostic tests on the ShipComputer
+        List<Integer> thermalDiagnosticProgram = CsvLineToInts(input.get(0));
+
+        // Tell the program to test the ships thermal radiator (system 5)
+        diagnosticCode = runDiagnosticsOnSystem(thermalDiagnosticProgram, 5);
+
+        return Integer.toString(diagnosticCode);
+    }
+
+    private int runDiagnosticsOnSystem(List<Integer> diagnosticProgram, int systemToDiagnose) {
+        LinkedList<Integer> inputInstruction = new LinkedList<>();
+        inputInstruction.add(systemToDiagnose);
+
+        ShipComputer computer = new ShipComputer(inputInstruction);
+        computer.executeProgram(diagnosticProgram);
+
+        LinkedList<Integer> diagnosticOutput = computer.outputBuffer();
+
+        // The system will output the result of each test and finally it will output a
+        // diagnostic code and halt.
+        int diagnosticCode = diagnosticOutput.removeLast();
+
+        if (!this.checkDiagnosticSuccessful(diagnosticOutput)) {
+            throw new RuntimeException("Test failure " + diagnosticOutput.toString());
+        }
+
+        return diagnosticCode;
     }
 
     private boolean checkDiagnosticSuccessful(LinkedList<Integer> testOutput) {
