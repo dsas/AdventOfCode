f7fba8fc7e8cd1974043d13babdaf1fcd399efba
Dean Sas <dean@deansas.org>
2020-12-05 09:37:18 +0000

    2020: Fix bug with day four part two
    The hgt validation rule said that heights between 50 and 58 inches were
    valid but they should not be. Thanks @PatrickRose.

--- 2020/src/Day04/index.js
+++ 2020/src/Day04/index.js
@@ -37,7 +37,7 @@ const requiredFieldsPresentAndCorrect = (passport) => {
     ['byr', /^(19[2-9][0-9]|200[1-2])$/], // four digits between 1920 and 2002 inclusive
     ['iyr', /^20(1[0-9]|20)$/], // four digits between 2010 and 2020 inclusive
     ['eyr', /^20(2[0-9]|30)$/], // four digits between 2020 and 2030 inclusive
-    ['hgt', /^1([5-8][0-9]|9[0-3])cm|^([5-6][0-9]|7[0-6])in$/], // if cm then between 150-193 inc, if inch then 59-76 inc. bit ugly
+    ['hgt', /^1([5-8][0-9]|9[0-3])cm|^(59|6[0-9]|7[0-6])in$/], // if cm then between 150-193 inc, if inch then 59-76 inc. bit ugly
     ['hcl', /^#[0-9a-f]{6}$/],  // six digit html colour essentially
     ['ecl', /^(amb|blu|brn|gry|grn|hzl|oth)$/],
     ['pid', /^[0-9]{9}$/],  // nine digit number including leading zeros
