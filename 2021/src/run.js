fd5b7e7e60f0ae576ed1d688d87126bb9f7e607f
Dean Sas <dean@deansas.org>
2021-12-02 19:14:19 +0000

    2021: Improved runner to prompt for day
    Improved the test runner to prompt for which day to run if it's not
    given.
    
    The primary motivation for this is it makes it easier to use the
    debugger in vscode, as I don't have to mess with the launch.json each
    time to set which day I'd like it to run.

--- 2021/src/run.js
+++ 2021/src/run.js
@@ -3,25 +3,62 @@
  */
 
 const fs = require('fs');
+const readline = require('readline');
+
+const dayDirectoryPrefix = 'Day';
+
+const question = (questionText) => {
+  return new Promise( (resolver) => {
+    rl.question(questionText, answer => { resolver(answer)})
+  });
+};
+
+const validateDay = (day) => {
+  if (isNaN(day)) {
+    return false;
+  }
+  if (! fs.existsSync(__dirname + '/' + dayDirectoryPrefix + day)) {
+    return false;
+  }
+  return true;
+}
+
+const normaliseDay = (day) => {
+  day = day.replace(/Day([\d]*)/i, '$1');
+  day = day.padStart(2, "0");
+  return day;
+};
+
+let rl = readline.createInterface( {input: process.stdin, output: process.stdout } );
+
+(async () => {
+
+let day;
 
 const args = process.argv.slice(2);
-if (!args.length) {
-  console.error("No arguments provided, provide the day number");
-  process.exit(1);
+if (args.length) {
+  day = args[0];
+  day = normaliseDay(day);
+  if (!validateDay(day)) {
+    console.error(day + ' is not a valid day');
+    process.exit(1);
+  }
+} else {
+  let validDay = false;
+  while (!validDay) {
+    const answerDay = await question('Which day should run? ')
+    day = normaliseDay(answerDay);
+    validDay = validateDay(day);
+    if (!validDay) {
+     console.info(answerDay + ' is not a valid day');
+    }
+  }
 }
-
-let day = args[0];
-day = day.replace(/Day([\d]*)/i, '$1');
-day = day.padStart(2, "0");
-
-if (isNaN(day)) {
-  console.error(day + " is not a number");
-  process.exit(1);
-}
+rl.close();
 
 console.info("Running for day " + day);
 
-const solver = require('./Day' + day);
+const solver = require('./' + dayDirectoryPrefix + day);
 
 let input = fs.readFileSync('./src/Day' + day + '/input.txt',  {"encoding":"ascii"});
 
@@ -30,3 +67,5 @@ console.log(solver.partOne(input))
 
 console.log("Part 2:");
 console.log(solver.partTwo(input));
+
+})();
