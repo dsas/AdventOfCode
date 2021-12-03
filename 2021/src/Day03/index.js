f6d99baee87e7b588b107e33013bd7f3780358df
Dean Sas <dean@deansas.org>
2021-12-03 22:42:28 +0000

    2021: day 3, add comments

--- 2021/src/Day03/index.js
+++ 2021/src/Day03/index.js
@@ -1,5 +1,6 @@
 /**
- * 
+ * Parse a list of bitfields, take the most common bit in each position to generate a new number and
+ * multiply that by the number generated from the least common bit
  */
 const partOne = (input) => {
     const parsed = parseInputString(input)
@@ -7,12 +8,16 @@ const partOne = (input) => {
     const most_common_threshold = parsed.length / 2;
     let gamma_binary = '';
 
+    // Generate a bitfield from the most common bit in each bit field position.
     for (let i = 0; i < bits_per_number; i++) {
         let count_of_zeros = parsed.filter((bit_field) => bit_field[i] == 0).length;
         gamma_binary += ( count_of_zeros > most_common_threshold ? '0' : '1');
     }
     let gamma = parseInt(gamma_binary, 2)
 
+    // Generate a bitfield from the least common bit in each bit field position.
+    // This must be the exact opposite of the value for the most common bit, so just
+    // (clumsily) invert it
     let epsilon_binary = gamma_binary.replace(/[01]/g, (b) => b == '0' ? '1' : '0')
     let epsilon = parseInt(epsilon_binary, 2);
 
@@ -20,11 +25,13 @@ const partOne = (input) => {
 }
 
 /**
- * 
+ * Filter the list of bitfields to find which bitfield has the most common bit in each position,
+ * repeat for least common bit and multiply the two
  */
 const partTwo = (input) => {
     const parsed = parseInputString(input)
 
+    // Find bitfield which always has the most common bit in each position
     let oxygen_candidates = parsed;
     let bit = 0;
     while (oxygen_candidates.length != 1) {
@@ -33,8 +40,9 @@ const partTwo = (input) => {
         oxygen_candidates = oxygen_candidates.filter( (candidate) => candidate[bit] == value_to_keep);
         bit++; 
     }
-    bit = 0;
 
+    // Find bitfield which always has the least common bit in each position
+    bit = 0;
     let co2_candidates = parsed;
     while (co2_candidates.length != 1) {
         let count_of_zeros = co2_candidates.filter(bit_field => bit_field[bit] == 0).length;
