736a3cc7a93a10e75d658d1b06005403abb87f3e
Dean Sas <dean@deansas.org>
2022-12-01 20:43:37 +0000

    2022: Copy last years framework

---
+++ 2022/README.md
@@ -0,0 +1,21 @@
+![Build status](https://github.com/dsas/AdventOfCode/workflows/2022%20build%20and%20test/badge.svg)
+# Advent of code 2022
+Solutions for [Advent of Code 2022](https://adventofcode.com/2022) challenge in javascript.
+
+
+# Install
+```
+git clone https://github.com/dsas/AdventOfCode.git
+cd AdventOfCode/2022
+yarn install
+```
+# Test
+```
+yarn run test
+yarn run lint
+```
+
+# Run
+```
+node . <Day no>
+```
---
+++ 2022/src/Day00/test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const solver = require('./');
+
+describe('Day 00 part one', function() {
+  it('should return 10 with input', function() {
+    let input = `3
+      5
+      2`;
+    assert.strictEqual(solver.partOne(input), 10);
+  });
+});
+
+describe('Day 00 part two', function() {
+  it('should return 10 with input', function() {
+    let input = `5
+      5
+      20`;
+    assert.strictEqual(solver.partTwo(input), 10);
+  });
+});
