7850faf55fc455b6eaaf99abbb7b70deb5fa9ebb
Dean Sas <dean@deansas.org>
2022-12-04 15:08:58 +0000

    2022: Reformat Day 04

--- 2022/src/Day04/index.js
+++ 2022/src/Day04/index.js
@@ -3,33 +3,51 @@
  * @param {string} input - A string of lines with comma separated ranges
  * @returns {number}
  */
-const partOne = (input) => {
-  const parsedInput = parseInputString(input);
-  return parsedInput.reduce( (containedPairCount, pair ) => {
-    if ((pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1])
-      || (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1])) {
-      containedPairCount++;
-    }
-    return containedPairCount;
-  }, 0);
-}
+const partOne = ( input ) => {
+	const parsedInput = parseInputString( input );
+	return parsedInput.reduce( ( containedPairCount, pair ) => {
+		if (
+			( pair[ 0 ][ 0 ] <= pair[ 1 ][ 0 ] &&
+				pair[ 0 ][ 1 ] >= pair[ 1 ][ 1 ] ) ||
+			( pair[ 1 ][ 0 ] <= pair[ 0 ][ 0 ] &&
+				pair[ 1 ][ 1 ] >= pair[ 0 ][ 1 ] )
+		) {
+			containedPairCount++;
+		}
+		return containedPairCount;
+	}, 0 );
+};
 
 /**
  * Find the number of partly overlapping pairs
  * @param {string} input - A string of lines with comma separated ranges
  * @returns {number}
  */
-const partTwo = (input) => {
-  const parsedInput = parseInputString(input);
-  return parsedInput.reduce((containedPairCount, pair) => {
-    if ((pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][0])
-      || (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][0])) {
-      containedPairCount++;
-    }
-    return containedPairCount;
-  }, 0);
-}
+const partTwo = ( input ) => {
+	const parsedInput = parseInputString( input );
+	return parsedInput.reduce( ( containedPairCount, pair ) => {
+		if (
+			( pair[ 0 ][ 0 ] <= pair[ 1 ][ 0 ] &&
+				pair[ 0 ][ 1 ] >= pair[ 1 ][ 0 ] ) ||
+			( pair[ 1 ][ 0 ] <= pair[ 0 ][ 0 ] &&
+				pair[ 1 ][ 1 ] >= pair[ 0 ][ 0 ] )
+		) {
+			containedPairCount++;
+		}
+		return containedPairCount;
+	}, 0 );
+};
 
-const parseInputString = (input) => input.trim().split("\n").map((line) => line.split(',').map((range) => range.split('-').map((num) => parseInt(num, 10))));
+const parseInputString = ( input ) =>
+	input
+		.trim()
+		.split( '\n' )
+		.map( ( line ) =>
+			line
+				.split( ',' )
+				.map( ( range ) =>
+					range.split( '-' ).map( ( num ) => parseInt( num, 10 ) )
+				)
+		);
 
-module.exports = {partOne, partTwo};
+module.exports = { partOne, partTwo };
