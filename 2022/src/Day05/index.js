8e9671af8ecbe9ec6ef92b067ca95726d10b654c
Dean Sas <dean@deansas.org>
2022-12-05 09:33:41 +0000

    2022: Add some docs for Day05

--- 2022/src/Day05/index.js
+++ 2022/src/Day05/index.js
@@ -1,4 +1,8 @@
 /**
+ * Given a string containing a list of stacks, and a string containing instructions, return the topmost cells of the stacks after the instructions have been executed
+ *
+ * The instructions are of the form of "move 3 from 1 to 2", which means to move the top 3 cells from stack 1 to stack 2.
+ * The cells can only be moved one at a time, so when moving three cells the order after being moved will be reversed.
  *
  *
  * @param {string} input
@@ -27,7 +31,10 @@ const partOne = ( input ) => {
 };
 
 /**
+ * Given a string containing a list of stacks, and a string containing instructions, return the topmost cells of the stacks after the instructions have been executed
  *
+ * The instructions are of the form of "move 3 from 1 to 2", which means to move the top 3 cells from stack 1 to stack 2.
+ * The cells in each instruction are moved simultaneously so their order after moving will be preserved.
  *
  * @param {string} input
  * @returns {string}
@@ -54,8 +61,21 @@ const partTwo = ( input ) => {
 	return topmostCellsAsString( stacks );
 };
 
+/**
+ * Just splits the input into two parts, the stacks and the instructions
+ * @param {string} input
+ * @returns {string}
+ */
 const parseInputString = ( input ) => input.trimEnd().split( '\n\n' );
 
+/**
+ * Parses the stacks from the input string into a 2D array
+ *
+ * The input string has the stacks in a jagged grid, where each grid cell is four characters wide.
+ * We just care about the cells that have a letter in them.
+ * @param {string} unparsedStacks
+ * @returns {string[][]}
+ */
 const parseStacks = ( unparsedStacks ) => {
 	let stacks = [];
 	unparsedStacks.split( '\n' ).forEach( ( line ) => {
@@ -80,6 +100,11 @@ const parseInstruction = ( unparsedInstruction ) => {
 		.slice( 1, 4 );
 };
 
+/**
+ * Given a 2D array return the last element of every array and concatenate them into a string
+ * @param {string[][]} stacks
+ * @returns {string}
+ */
 const topmostCellsAsString = ( stacks ) => {
 	const topmostCells = stacks.map( ( stack ) => stack.slice( -1 ) );
 	return topmostCells.join( '' );
