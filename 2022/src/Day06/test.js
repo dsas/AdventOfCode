d6cdb25ef7d045f837ca92503ce490f115c7ccbd
Dean Sas <dean@deansas.org>
2022-12-06 23:49:23 +0000

    2022: More test cases for Day06

--- 2022/src/Day06/test.js
+++ 2022/src/Day06/test.js
@@ -4,8 +4,19 @@ const solver = require( './' );
 describe( 'Day 06 part one', function () {
 	it( 'should return 5 with input', function () {
 		let input = `bvwbjplbgvbhsrlpgdmjqwftvncz`;
-
 		assert.strictEqual( solver.partOne( input ), 5 );
+	} );
+	it( 'should return 6  with input', function () {
+		let input = `nppdvjthqldpwncqszvftbrmjlhg`;
+		assert.strictEqual( solver.partOne( input ), 6 );
+	} );
+	it( 'should return 10  with input', function () {
+		let input = `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`;
+		assert.strictEqual( solver.partOne( input ), 10 );
+	} );
+	it( 'should return 11  with input', function () {
+		let input = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`;
+		assert.strictEqual( solver.partOne( input ), 11 );
 	} );
 } );
 
