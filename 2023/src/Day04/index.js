b331043ffe5c53fda569f1ff0b71d825a09fb0d2
Dean Sas <dean@deansas.org>
2023-12-05 00:45:05 +0000

    2023: Solve Day 04 part two

--- 2023/src/Day04/index.js
+++ 2023/src/Day04/index.js
@@ -1,3 +1,13 @@
+/**
+ * Find the sum of each cards score
+ *
+ * The score is calculated by counting how many numbers on the card appear
+ * in both sections, subtracting 1 from that number, and raising 2 to the
+ * power of that number.
+ *
+ * @param {*} input
+ * @returns
+ */
 const partOne = ( input ) => {
   const cards = parseInputString( input );
   return cards.reduce( ( sum, card ) => {
@@ -9,24 +19,55 @@ const partOne = ( input ) => {
     return sum + 2 ** ( matchingNumbers.length - 1);
   }, 0);
 }
+
+/**
+ * Find how many cards get processed
+ *
+ * Every number on the card that is repeated in both sections means another
+ * copy of the card is added to the deck. i.e. when processing Card 1, if it
+ * has two repeats then a copy of card 2 and a copy of card 3 are added to
+ * the deck. Copies of a card are always processed sequentially, by card
+ * number.
+ *
+ *
+ * @param {string} input
+ * @returns
+ */
 const partTwo = ( input ) => {
-  return undefined;
+  const cards = parseInputString( input );
+
+  // Every card has at least one copy
+  const cardCopies = new Array( cards.length ).fill( 1 );
+
+  for ( let i = 0; i < cards.length; i++ ) {
+    const card = cards[ i ];
+    const { winningNumbers, cardNumbers } = card;
+    const matchingNumbers = winningNumbers.filter( number => cardNumbers.includes( number ) ).length;
+
+    // If this card means there are matching numbers, then each copy of this
+    // card will have the same effect.
+    for ( let j = i + 1; j <= i + matchingNumbers; j++ ) {
+      cardCopies[j] += cardCopies[i];
+    }
+  }
+
+  return cardCopies.reduce( ( sum, copies ) => sum + copies, 0 );
 }
 
 const parseInputString = (input) => {
   const cards = input.trim()
     .split( "\n" )
-    .map(line => line.match(/Card\s+\d+: (?<winning_numbers>.*) \| (?<card_numbers>.*)/).groups );
+    .map( line => line.match( /Card\s+\d+: (?<winning_numbers>.*) \| (?<card_numbers>.*)/ ).groups );
   return cards.map( card => {
     return {
       winningNumbers: card.winning_numbers.split(" ")
         .map( number => parseInt( number ) )
         .filter( number => !Number.isNaN( number ) ),
       cardNumbers: card.card_numbers.split(" ")
-        .map(number => parseInt(number))
-        .filter( number => !Number.isNaN( number ) )
+        .map( number => parseInt( number ) )
+        .filter( number => !Number.isNaN( number ) ),
     }
   } );
 }
 
-module.exports = {partOne, partTwo};
+module.exports = { partOne, partTwo };
--- 2023/src/Day04/test.js
+++ 2023/src/Day04/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const solver = require('./');
 
 describe( 'Day 04 part one', function() {
-  it('should return 13 with input', function() {
+  it( 'should return 13 with input', function() {
     let input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -14,8 +14,13 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 } );
 
 describe( 'Day 04 part two', function() {
-  it( 'should return 10 with input', function() {
-    let input = ``;
-    assert.strictEqual( solver.partTwo( input ), 10 );
+  it( 'should return 30 with input', function() {
+    let input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
+    assert.strictEqual( solver.partTwo( input ), 30 );
   } );
 } );
