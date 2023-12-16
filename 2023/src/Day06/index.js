824d7ab27b707203f43d150a25963bf918d479f7
Dean Sas <dean@deansas.org>
2023-12-16 15:47:22 +0000

    2023: Solve Day06 part two

--- 2023/src/Day06/index.js
+++ 2023/src/Day06/index.js
@@ -1,27 +1,35 @@
 const partOne = ( input ) => {
   const [ times, distances ] = parseInputString( input );
-  let waysFoundPerRace = [];
+  let totalWays = 1;
 
   for ( let race = 0; race < times.length; race++ ) {
-    let timeAvailable = times[race];
-    let distanceToBeat = distances[race];
-    let waysFoundCount = 0;
-
-    for ( let chargeTime = 0; chargeTime < timeAvailable; chargeTime++ ) {
-      let distanceTravelled = chargeTime * ( timeAvailable - chargeTime );
-      if ( distanceTravelled > distanceToBeat ) {
-        waysFoundCount++;
-      }
-    }
-    waysFoundPerRace.push( waysFoundCount );
+    const waysFoundCount = findWays( times[race], distances[race] );
+    totalWays *= waysFoundCount;
   }
 
-  return waysFoundPerRace.reduce( ( accumulator, current ) => accumulator * current );
+  return totalWays;
 
 }
 const partTwo = ( input ) => {
+  const [ times, distances ] = parseInputString( input );
+  const timeAvailable = parseInt( times.join( '' ) );
+  const distanceToBeat = parseInt( distances.join( '' ) );
+  return findWays( timeAvailable, distanceToBeat );
+
 }
 
 const parseInputString = ( input ) => input.trim().split( "\n" ).map( s => s.split( /\s+/ ).slice( 1 ).map( n => parseInt( n ) ) );
+
+
+const findWays = ( timeAvailable, distanceToBeat ) => {
+  let waysFoundCount = 0;
+  for ( let chargeTime = 0; chargeTime < timeAvailable; chargeTime++ ) {
+    let distanceTravelled = chargeTime * ( timeAvailable - chargeTime );
+    if ( distanceTravelled > distanceToBeat ) {
+      waysFoundCount++;
+    }
+  }
+  return waysFoundCount;
+}
 
 module.exports = { partOne, partTwo };
